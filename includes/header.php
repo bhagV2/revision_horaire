b21f5cdf26f60d88a0dcc9c56c61e76b20265841
Bhag <bhagya.mschc@eduge.ch>
2026-09-03 11:21:31 +0200

    Création de la base du header

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<header>
+    <nav>
+        <li><a href="pages/classes.php">Classes</a></li>
+        <li><a href="pages/cours.php">Cours</a></li>
+        <li><a href="pages/horaire.php">Horaire</a></li>
+    </nav>
+</header>
