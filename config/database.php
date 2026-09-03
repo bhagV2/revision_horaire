d114b1c40b54e8d4820ec49014e7972f0e732052
Bhag <bhagya.mschc@eduge.ch>
2026-09-03 11:35:21 +0200

    Connection base de données

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Affichage d'une liste d'animaux
+ * @author M.Bhagya
+ */
+define("DB_HOST", "localhost");
+define("DB_NAME", "horaire");
+define("DB_CHAR", "utf8");
+define("DB_USER", "root");
+define("DB_PASS", "VaccineSecret");
---
+++ connexion/db.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Affichage d'une liste d'animaux
+ * @author M.Bhagya
+ */
+require_once ROOT . "/php/config-secret.php";
+
+function db() : PDO
+{
+    static $db = null;
+
+    if ($db === null) {
+        // Se connecter à la base de données
+        $db = new PDO(
+            "mysql:host=". DB_HOST .";dbname=". DB_NAME .";charset=". DB_CHAR, DB_USER, DB_PASS
+        );
+
+        // Configurer la connexion à la DB
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+    
+    return $db;
+}
