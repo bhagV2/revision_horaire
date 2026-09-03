802484f6dd8f0c2f1d37c501344a3f0f1ac9cb5a
Bhag <bhagya.mschc@eduge.ch>
2026-09-03 11:41:09 +0200

    Implémentation bootstrap

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceuil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <?php include("includes/header.php"); ?>
@@ -11,4 +12,5 @@
     <p>Vous êtes arrivés sur un site qui affiche des horaires.</p>
     <?php include("includes/footer.php"); ?>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
