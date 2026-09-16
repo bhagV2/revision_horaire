a2e390460006d1ac41d458cc4976218a84d0b20e
bhagya-mschc <bhagya.mschc@eduge.ch>
2026-09-16 21:32:06 +0200

    faute de frappe

--- sql/user.sql
+++ sql/user.sql
@@ -2,7 +2,7 @@
 -- User: `horaireUSer`
 --
 DROP USER IF EXISTS 'horaireUser'@'localhost';
-CREATE USER 'horaireUser'@'localhost' IDENTIFIED BY 'horaireSuper';
+CREATE USER 'horaireUser'@'localhost' IDENTIFIED BY 'HoraireSuper';
 
 GRANT INSERT ON horaire.* TO 'horaireUser'@'localhost';
 GRANT SELECT ON horaire.* TO 'horaireUser'@'localhost';
@@ -10,7 +10,7 @@ GRANT UPDATE ON horaire.* TO 'horaireUser'@'localhost';
 GRANT DELETE ON horaire.* TO 'horaireUser'@'localhost';
 
 DROP USER IF EXISTS 'horaireUser'@'%';
-CREATE USER 'horaireUser'@'%' IDENTIFIED BY 'horaireSuper';
+CREATE USER 'horaireUser'@'%' IDENTIFIED BY 'HoraireSuper';
 
 GRANT INSERT ON horaire.* TO 'horaireUser'@'%';
 GRANT SELECT ON horaire.* TO 'horaireUser'@'%';
