5e9f86d547708a50a6d422404e94c47030275950
bhagya-mschc <bhagya.mschc@eduge.ch>
2026-09-16 18:47:00 +0200

    Header avec une nav-bar qui fonctionne

--- includes/header.php
+++ includes/header.php
@@ -1,24 +1,26 @@
 <?php
 /**
- * En-tête
+ * Barre de navigation
  * @author M.Bhagya
  */
+
+$root = str_replace('/includes', '', dirname($_SERVER['SCRIPT_NAME']));
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Projet Horaire - CFPT</title>
-</head>
-<body>
-<header>
-    <nav>
-        <ul>
-            <li><a href="/pages/classes.php">Classes</a></li>
-            <li><a href="/pages/cours.php">Cours</a></li>
-            <li><a href="/pages/horaire.php">Horaire</a></li>
+<header class="bg-light border-bottom py-2">
+    <nav class="container">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $root ?>/index.php">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $root ?>/pages/classes.php">Classes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $root ?>/pages/cours.php">Cours</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $root ?>/pages/horaire.php">Horaire</a>
+            </li>
         </ul>
     </nav>
 </header>
-<hr>
-<main>
