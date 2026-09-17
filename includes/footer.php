210d8bc4b0177e67dfcef14c60bf9cd855d138f7
Bhag <bhagya.mschc@eduge.ch>
2026-09-17 09:18:51 +0200

    Correction du lien bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -9,6 +9,6 @@ $root = str_replace('/includes', '', dirname($_SERVER['SCRIPT_NAME']));
 <footer class="text-center py-4 mt-5 border-top text-muted">
     <p>© <?= date('Y') ?> - CFPT - M.Bhagya</p>
 </footer>
-<script src="<?= $root ?>/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -4,18 +4,19 @@
  * @author M.Bhagya
  */
 $root = str_replace('/includes', '', dirname($_SERVER['SCRIPT_NAME']));
+$root = str_replace('/pages', '', dirname($_SERVER['SCRIPT_NAME']));
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Projet Horaire - CFPT</title>
-    <link href="<?= $root ?>/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
 <header class="bg-white border-bottom py-2 mb-4 shadow-sm">
     <nav class="container">
-        <ul class="nav">
+        <ul class="nav flex">
             <li class="nav-item"><a class="nav-link" href="<?= $root ?>/index.php">Accueil</a></li>
             <li class="nav-item"><a class="nav-link" href="<?= $root ?>/pages/classes.php">Classes</a></li>
             <li class="nav-item"><a class="nav-link" href="<?= $root ?>/pages/cours.php">Cours</a></li>
